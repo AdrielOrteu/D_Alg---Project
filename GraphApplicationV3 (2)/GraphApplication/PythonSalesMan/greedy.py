06d6f6ec30515755c794272fc1c3435183011a94
Youknowm22 <diogo.cabrita.teixeira@gmail.com>
2026-03-23 15:26:54 +0100

    Logica greedy sence cami

--- GraphApplicationV3 (2)/GraphApplication/PythonSalesMan/greedy.py
+++ GraphApplicationV3 (2)/GraphApplication/PythonSalesMan/greedy.py
@@ -1,37 +1,19 @@
-
+import graph
 import math
 import sys
 import queue
 import heapq
+from tracemalloc import start
 
 # SalesmanTrackGreedy ==========================================================
 
 def dijkstra (g, start, Visitas):
-
-    if not g.Vertices or start is None:
-        return None
-        
-    g.set_Edge_dict()
-    g.set_Dijkstra_Distance(start)
-    g.set_Dijkstra_Visit()
-    primer = True
-    while Visitas:
-        fora = 0
-        if primera :
-            cua_prioritat = []
-            heapq.heappush(cua_prioritat, (0.0, start))
-            primera = False
-        else:
-            maxim = sys.float_info.max
-            node = None
-            for vertex in Visitas:
-                if vertex.DijkstraDistance < maxim:
-                    maxim = vertex.DijkstraDistance
-                    node = vertex
-            Visitas.remove(node)
-            cua_prioritat = []
-            heapq.heappush(cua_prioritat, (0.0, node))
-        while cua_prioritat and fora < len(Visitas):
+        cua_prioritat = []
+        camino = []
+        g.set_Dijkstra_Distance(start)
+        g.set_Dijkstra_Visit()
+        heapq.heappush(cua_prioritat, (0.0, start))
+        while cua_prioritat:
             dist_actual, actual = heapq.heappop(cua_prioritat)
             if actual.DijktraVisit:
                 continue
@@ -49,8 +31,24 @@ def dijkstra (g, start, Visitas):
                         destination.DijkstraDistance = nova_distancia
                         # Afegim a la cua amb heapq
                         heapq.heappush(cua_prioritat, (nova_distancia, destination))
-                        
-        return None
-def SalesmanTrackGreedy(g,visits):
+        max_dist = sys.float_info.max
+        next_jump = None
+        for node in Visitas:
+             if node.DijkstraDistance < max_dist:
+                max_dist = node.DijkstraDistance
+                next_jump = node
 
-    return graph.Track(g)
+        return next_jump , camino
+    
+def SalesmanTrackGreedy(g,visits):
+        if not g.Vertices or visits is None:
+            return None
+        g.set_Edge_dict()
+        next_jump = visits[0]
+        camino_total = []
+        while visits:
+            visits.remove(next_jump)
+            next_jump, camino = dijkstra(g, visits[0], visits)
+            camino_total.extend(camino)
+
+        return graph.Track(g)
