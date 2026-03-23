767646b1c857144e3431be7b47cf152e6390d2b5
Youknowm22 <diogo.cabrita.teixeira@gmail.com>
2026-03-23 14:51:47 +0100

    Canvis sence conflict final

--- GraphApplicationV3 (2)/GraphApplication/PythonSalesMan/dijkstra.py
+++ GraphApplicationV3 (2)/GraphApplication/PythonSalesMan/dijkstra.py
@@ -6,6 +6,8 @@ import queue
 # Dijkstra =====================================================================
 
 def Dijkstra(g,start):
+	pass
+
 def DijkstraQueue(g ,start ):
 	g.set_Edge_dict()
 	if g.Vertices == [] or start == None:
@@ -25,7 +27,6 @@ def DijkstraQueue(g ,start ):
 					if acumulada < destination.DijkstraDistance:
 						destination.DijkstraDistance = acumulada
 						cua_distancias.put((acumulada, destination))
-	return None
 	return None
 # DijkstraQueue ================================================================
 
@@ -49,3 +50,31 @@ def DijkstraQueue(g ,start ):
 						destination.DijkstraDistance = acumulada
 						cua_distancias.put((acumulada, destination))
 	return None
+
+if __name__ == "__main__":
+	vertices = [
+		("Start", -17.03, 160.763),
+		("V0002", 316.757, 177.112),
+		("V0003", 444.823, 335.15),
+		("V0004", -6.13079, 284.741)
+	]
+	edges = [
+		("E0001", 0, "Start", "V0004"),
+		("E0003", 0, "V0004", "V0002"),
+		("E0005", 0, "Start", "V0003"),
+		("E0007", 0, "V0002", "V0003"),
+		("E0009", 0, "Start", "V0002")
+	]
+	
+	from graph import Graph, Vertex, Edge
+	g = Graph()
+	for v in vertices:
+		g.NewVertex(v[0], v[1], v[2])
+	for e in edges:
+		g.NewEdge(e[0], e[1], e[2], e[3])
+	g.SetDistancesToEdgeLength()
+	start = g.FindVertex("Start", g.Vertices[0])
+	
+	Dijkstra(g, start)
+	#DijkstraQueue(g, start)
+	print(g)
