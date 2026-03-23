326323062047cee22fc6f8055a2c7411fbef00ba
Youknowm22 <diogo.cabrita.teixeira@gmail.com>
2026-03-23 14:03:20 +0100

    Commit dijktra funciona para graf10

--- GraphApplicationV3 (2)/GraphApplication/PythonSalesMan/dijkstra.py
+++ GraphApplicationV3 (2)/GraphApplication/PythonSalesMan/dijkstra.py
@@ -15,17 +15,17 @@ def DijkstraQueue(g ,start ):
 	g.set_Dijkstra_Distance(start)
 	g.set_Dijkstra_Visit()
 	cua_distancias = queue.PriorityQueue()
-	cua_distancias.put((start, 0))
+	cua_distancias.put((0,start))
 	acumulada = 0
 	while cua_distancias.empty() == False:
 		actual = cua_distancias.get()
-		if actual[0].DijktraVisit == True:
+		if actual[1].DijktraVisit == True:
 			continue
-		actual[0].DijktraVisit = True
-		acumulada += actual[1]
-		for destination, edge in g.get_Edges(actual[0]):
+		actual[1].DijktraVisit = True
+		for destination, edge in g.get_Edges(actual[1]):
 				if destination.DijktraVisit == False:
-					cua_distancias.put((destination, edge.Length))
-					if acumulada + edge.Length < destination.DijkstraDistance:
-						destination.DijkstraDistance = acumulada + edge.Length
+					acumulada = actual[1].DijkstraDistance + edge.Length
+					if acumulada < destination.DijkstraDistance:
+						destination.DijkstraDistance = acumulada
+						cua_distancias.put((edge.Length, destination))
 	return None
