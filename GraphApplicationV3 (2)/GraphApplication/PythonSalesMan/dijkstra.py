9cbb6d159716736659f66b99054e3dccfa0425ab
Youknowm22 <diogo.cabrita.teixeira@gmail.com>
2026-03-23 14:50:41 +0100

    Canvis distktra tots menys 1

--- GraphApplicationV3 (2)/GraphApplication/PythonSalesMan/dijkstra.py
+++ GraphApplicationV3 (2)/GraphApplication/PythonSalesMan/dijkstra.py
@@ -6,22 +6,26 @@ import queue
 # Dijkstra =====================================================================
 
 def Dijkstra(g,start):
+def DijkstraQueue(g ,start ):
 	g.set_Edge_dict()
-	g.set_Dijkstra_Distance(start)
+	if g.Vertices == [] or start == None:
+		return None
 	g.set_Dijkstra_Visit()
 	cua_distancias = queue.PriorityQueue()
-	cua_distancias.put((start, 0))
+	cua_distancias.put((0,start))
 	acumulada = 0
-	while cua_distancias:
-		actual = cua_distancias.get()
-		if actual[0].DijktraVisit == False:
-			actual[0].DijktraVisit
-			acumulada += actual[1]
-			for destination, edge in g.get_Edges(actual):
-				if destination.DijkstraVisit == False:
-					cua_distancias.put(edge.Origin, edge.Length)
-					if acumulada + edge.Length < destination.DijkstraDistance:
-						destination.DijkstraDistance = acumulada + edge.Length
+	while cua_distancias.empty() == False:
+		dist , actual = cua_distancias.get()
+		if actual.DijktraVisit == True:
+			continue
+		actual.DijktraVisit = True
+		for destination, edge in g.get_Edges(actual):
+				if destination.DijktraVisit == False:
+					acumulada = dist + edge.Length
+					if acumulada < destination.DijkstraDistance:
+						destination.DijkstraDistance = acumulada
+						cua_distancias.put((acumulada, destination))
+	return None
 	return None
 # DijkstraQueue ================================================================
 
